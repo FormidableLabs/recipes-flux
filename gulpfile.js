4f160e481b2b5b2c880bd006bb0cbf1bafac13ff
Colin Megill <colinmegill@gmail.com>
2015-01-12 02:22:18 -0800

    experimenting with terser logging

--- gulpfile.js
+++ gulpfile.js
@@ -83,11 +83,11 @@ gulp.task("build:dev", function (done) {
   webpack(buildDevCfg).run(function (err, stats) {
     if (err) { throw new gutil.PluginError("webpack", err); }
 
-    gutil.log("[webpack]", stats.toString({
-      hash: true,
-      colors: true,
-      cached: false
-    }));
+    // gutil.log("[webpack]", stats.toString({
+    //   hash: true,
+    //   colors: true,
+    //   cached: false
+    // }));
 
     done();
   });
