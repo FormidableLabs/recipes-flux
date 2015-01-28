d5333358c700950bbad6309f2bfc687c16f1403e
Alex Lande <alexlande@gmail.com>
2015-01-28 12:58:17 -0800

    Fix input cursor jumping bug

--- client/components/ingredient-form-input.jsx
+++ client/components/ingredient-form-input.jsx
@@ -13,6 +13,12 @@ var RadiumButton = require("../radium/components/button");
 var RadiumInput = require("../radium/components/input");
 
 var IngredientFormInput = React.createClass({
+  getInitialState: function () {
+    return {
+      value: null
+    };
+  },
+
   getDefaultProps: function () {
     return {
       labelHidden: true
@@ -20,18 +26,27 @@ var IngredientFormInput = React.createClass({
   },
 
   handleChange: function () {
+    var newValue = this.refs.input.getInputElement().value;
+
     RecipeActions.inputChanged({
       _id: this.props._id,
       accessor: this.props.accessor,
       index: this.props.index,
-      value: this.refs.input.getInputElement().value
+      value: newValue
+    });
+
+    this.setState({
+      value: newValue
     });
   },
 
   render: function () {
+    var value = this.state.value || this.props.value;
+
     return (/*jshint ignore:start*/
       <RadiumInput
         {...this.props}
+        value={value}
         ref="input"
         onChange={this.handleChange}
         />
